dce31edcd39f5e7deda9edd3ad15ab91effc80d8
LogiC <mohamedelheimer0@gmail.com>
2024-05-03 19:48:19 +0100

    all

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function add"""
+
+
+def add(a: float, b: float) -> float:
+    """return the addition of a and b"""
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+tasks about python variable annotations
