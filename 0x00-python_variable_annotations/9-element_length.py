ef62d929dec4dc9cfe7689db474c18aeaf876517
LogiC <mohamedelheimer0@gmail.com>
2024-05-04 02:30:44 +0100

    all

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Annotate the below function’s parameters and return values
+ with the appropriate types"""
+from typing import Tuple, List, Sequence, Iterable
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
