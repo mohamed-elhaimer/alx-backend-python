c2b6d3c94968752e535b614b752f195fea0cde9b
LogiC <mohamedelheimer0@gmail.com>
2024-05-07 17:58:13 +0100

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,6 +13,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     Returns:
         nothing
     """
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
